798b5730954f321840a784fd67e03a5a73fc3a2c
Nat <robarge.natasha7@gmail.com>
2020-01-31 17:08:25 -0600

    remove console log

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,3 @@
-console.log("Hey, thanks for looking through the code. This site is still needing feedback and I'm still working on it so if you have any feedback, I'd love to hear your thoughts. I convinced a UX designer to help me out with my portfolio and if you'd like me to direct her to you, I can do so because she's currently up for hire as well. ");
-console.log("----------------------------------------------------------------------------------------");
-
 $(document).on('click', 'a[href^="#"]', function (event) {
     event.preventDefault();
 
